ed4d6e632d8ea5a9d02f0d0b6180a8c9f0f94dd1
Vlad <simvlad002@gmail.com>
2019-03-01 16:23:42 +0100

    GHappyTest completed, found bug in GHappy

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,59 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class GHappyTest {
+
+  private GHappy test;
+
+  @BeforeEach
+  void initTest() {
+
+    this.test = new GHappy();
+  }
+
+  @Test
+  public void oneGroupOfGs() {
+
+    boolean result = test.gHappy("Sldkggwoqeqzxcp");
+    Assertions.assertTrue(result);
+  }
+
+  @Test
+  public void moreGroupsOfGs(){
+
+    boolean result = test.gHappy("sdajkggaejkwwfggsaeoiw");
+    Assertions.assertTrue(result);
+  }
+
+  @Test
+  public void noGs(){
+
+    boolean result = test.gHappy("asdkljqwepoasdnczm,sleo");
+    Assertions.assertTrue(result);
+  }
+
+  @Test
+  public void oneG(){
+
+    boolean result = test.gHappy("kwepoigsaooieqw");
+    Assertions.assertFalse(result);
+  }
+
+  @Test
+  public void moreGsNotInGroup(){
+
+    boolean result = test.gHappy("kwegpoigsaooieqw");
+    Assertions.assertFalse(result);
+  }
+
+  @Test
+  public void onlyOneG(){
+
+    boolean result = test.gHappy("g");
+    Assertions.assertFalse(result);
+  }
+
 }
